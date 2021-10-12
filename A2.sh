d3b3170dc7876d224178ddb1548264b6e1680f06
kwinn <kdanforth@student.ncmcih.edu>
2021-10-12 13:54:52 +0000

    This completes the assignment and when tested seems to work I left in but commented out my attempt to change the owner before chmod for each file and directory because I do not have permission to do this. I found that maybe it was not nessisary to change the owner but I thought that the promt asked that root have full privilages. So, I wanted to make root the owner to do this.

--- A2.sh
+++ A2.sh
@@ -5,41 +5,40 @@ cd root
 mkdir images
 mkdir scripts
 touch index.html
-cp /var/www/images/logo1.png /images/logo1.png
-cp /var/www/images/logo2.png /images/logo2.png
-sudo chown root:root index.html
-sudo chmod 704 index.html
+cp /var/www/images/logo1.png images/logo1.png
+cp /var/www/images/logo2.png images/logo2.png
+#chown root:root index.html
+chmod 704 index.html
 
-sudo chown root:root /images
-sudo chmod 704 /images
+#chown root:root images
+chmod 704 images
 
-sudo chown root:root /scripts 
-sudo chmod 704 /scripts
- 
-sudo chown root:root /root
-sudo chmod 704 /root
- 
-sudo chown root:root /sandbox
-sudo chmod 704 /sandbox
+#chown root:root scripts 
+chmod 704 scripts
+ cd ../
+#chown root:root root
+chmod 704 root
+ cd ../
+#chown root:root sandbox
+chmod 704 sandbox
 
-cd images
+cd sandbox/root/images
 
-sudo chown root:root logo1.png
-sudo chmod 704 logo1.png
+#chown root:root logo1.png
+chmod 704 logo1.png
 
-sudo chown root:root logo2.png
-sudo chmod 704 logo2.png
+#chown root:root logo2.png
+chmod 704 logo2.png
 cd ../
 cd ../
-cd ../
-ls -R sandbox > /root/web-listing
-ls -R /var/www/images | grep -i *.png > /root/avail-images
-cd ./sandbox/root
-sudo chown root:root web-listing
-sudo chmod 704 web-listing
+ls -R > root/web-listing
+ls -R /var/www/images | grep -i ".png" > root/avail-images
+cd root
+#chown root:root web-listing
+chmod 704 web-listing
 
-sudo chown root:root avail-images
-sudo chmod 704 avail-images
+#chown root:root avail-images
+chmod 704 avail-images
 
  
 
